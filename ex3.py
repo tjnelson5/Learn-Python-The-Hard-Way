98c72c2ef71888c543b67c45d7042ddb2ec5c520
tjnelson5 <thomasnelson1@gmail.com>
2015-03-01 19:02:45 -0600

    First version of Ex 3

---
+++ ex3.py
@@ -0,0 +1,27 @@
+# This line prints a statement that we are about to count our chickens
+print "I will now count my chickens:"
+
+# This will print the word "Hens" and then the number of hens
+print "Hens", 25 + 30 / 6
+# This will print the work "Roosters" and then the number of Roosters
+# multiply has higher precedence than mod, so answer will be 100 - 3 i.e. 97
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
