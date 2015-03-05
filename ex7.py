7483dc69bf05b67d8074904fb4d5a209813a8bca
tjnelson5 <thomasnelson1@gmail.com>
2015-03-05 07:05:42 -0600

    Added ex 7 with a few modifications from lpthw original

---
+++ ex7.py
@@ -0,0 +1,24 @@
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print ".!" * 5 + ".!" * 5 # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. Try removing it to see what happens
+
+# The comma is included here to extend the print statement over two lines
+# This is done to keep the line of code at 80 characters (a style thing)
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
