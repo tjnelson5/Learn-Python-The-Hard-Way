a61b2dd8b92216911e717adb5a5628a55f12ab35
tjnelson5 <thomasnelson1@gmail.com>
2015-03-04 22:46:46 -0600

    Added comments to each variable assignment

--- ex4.py
+++ ex4.py
@@ -1,10 +1,25 @@
+# cars available
 cars = 100
+
+# number of seats in each car
 space_in_a_car = 4
+
+# number of drivers available
 drivers = 30
+
+# number of people hoping to carpool
 passengers = 90
+
+# number of empty cars
 cars_not_driven = cars - drivers
+
+# number of cars to be driven
 cars_driven = drivers
+
+# total number of people who can carpool
 carpool_capacity = cars_driven * space_in_a_car
+
+# number of people per car 
 average_passengers_per_car = passengers / cars_driven
 
 print "There are", cars, "cars available."
